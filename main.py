aa72f233b5a9b497b6a2ba4302065f3a3ec271e0
Javier Ramirez <javierram8a@gmail.com>
2023-09-29 13:29:02 -0600

    Correción de colores interfaz

--- main.py
+++ main.py
@@ -6,34 +6,35 @@ class interface:
         # Configuración del frame principal 
         self.root = tk.Tk()
         self.root.wm_attributes('-zoomed', 1)
-        self.root.grid_rowconfigure(0, weight=2, uniform="rows_g1")
-        self.root.grid_rowconfigure(1, weight=2, uniform="rows_g1")
+        self.root.grid_rowconfigure(0, weight=3, uniform="rows_g1")
+        self.root.grid_rowconfigure(1, weight=3, uniform="rows_g1")
         self.root.grid_columnconfigure(0, weight=3,  uniform="cols_g1")
         self.root.grid_columnconfigure(1, weight=1,  uniform="cols_g1")
 
         self.root.grid_rowconfigure(2, weight=1, uniform="rows_g1")
         
         # Frame1: Es para el som
-        fm1 = tk.Frame(self.root, bg='red')
+        fm1 = tk.Frame(self.root, bg='white',highlightbackground="black", highlightthickness=2)
         fm1.grid(row=0, column=0, rowspan=2, sticky='nsew')
-
+        # Funcion para graficar SOM
+        
         # Frame2: Una palabra
-        fm2 = tk.Frame(self.root, bg='blue')
+        fm2 = tk.Frame(self.root, bg='white',highlightbackground="black", highlightthickness=2)
         fm2.grid(row=0, column=1, sticky='nsew')
 
-        # Entique
-        tk.Label(fm2, text='One word', bg='blue').pack()
+        # Etiqueta
+        tk.Label(fm2, text='One word', bg='white').pack()
         # Entrada para una palabra
         tk.Entry(fm2).pack()
         # botón para una palabra
         tk.Button(fm2, text='Search').pack()
 
         # Frame3: Dos palabra
-        fm3 = tk.Frame(self.root, bg='green')
+        fm3 = tk.Frame(self.root, bg='white',highlightbackground="black", highlightthickness=2)
         fm3.grid(row=1, column=1, sticky='nsew')
 
         # Entique
-        tk.Label(fm3, text='Two words', bg='blue').pack()
+        tk.Label(fm3, text='Two words', bg='white').pack()
         # Entrada para una palabra
         tk.Entry(fm3).pack()
         # Entrada para una palabra
@@ -42,19 +43,16 @@ class interface:
         tk.Button(fm3, text='Search').pack()
 
         # Frame4: Meter archivo y entrenar el som.
-        fm4 = tk.Frame(self.root, bg='pink')
+        fm4 = tk.Frame(self.root, bg='white',highlightbackground="black", highlightthickness=2)
         fm4.grid(row=2, column=0, columnspan=2, sticky='nsew')
 
         # Entique
-        tk.Label(fm4, text='Algo va aqui', bg='blue').pack()
+        tk.Label(fm4, text='Algo va aqui', bg='white').pack()
         # Entrada para una palabra
         #tk.filedialog(fm4, mode='r').pack()
         # botón para una palabra
         tk.Button(fm4, text='Submit').pack()
 
         self.root.mainloop()
-
-        
-        
         
 a = interface()
